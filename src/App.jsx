e85e94125d44f140eb1f369b50ab0be943b9e4e8
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-11-06 19:19:52 -0600

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Navbar } from "./components/navbar/Navbar.jsx";
+import { Home } from "./components/home/Home.jsx";
 
 function App() {
-  return <div className="App">Shepherd's Pies</div>;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar/>
+              <Outlet />
+            </>}
+        >
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
