f5741f76131e46d14cec56a9fca07f7b5de9bf25
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-11-20 20:08:00 -0600

    Added some layout styling to reports view

--- src/components/reports/Reports.jsx
+++ src/components/reports/Reports.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Card, CardContent, CardHeader, MenuItem, Select, Typography } from "@mui/material"
+import { Box, Button, ButtonGroup, Card, CardContent, CardHeader, List, ListItem, ListItemText, MenuItem, Select, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { getAllOrders } from "../../services/orders/ordersServices.js";
 import { getDetailedOrders, getReportValues } from "../../services/reports/reportServices.js";
@@ -57,70 +57,78 @@ export const Reports = () => {
 
     return (
         <>
-        <Box sx={{display: "flex", justifyContent: "center", marginTop: "1rem"}}>
-            <Box sx={{ display: "flex", gap: 2, alignItems: "center", flexWrap: "wrap" }}>
-                <Select
-                    value={selectedYear}
-                    onChange={e => setSelectedYear(e.target.value)}
-                    sx={{ minWidth: 100 }}
-                    size="small"
-                >
-                    {years}
-                </Select>
-                <ButtonGroup sx={{display: "flex", flexWrap: "wrap"}}>
-                    {months.map((month, idx) => (
-                        <Button
-                            key={month}
-                            variant={selectedMonth === idx ? "contained" : "outlined"}
-                            onClick={() => setSelectedMonth(idx)}
-                        >
-                            {month.slice(0,3)}
-                        </Button>
-                    ))}
-                </ButtonGroup>
+        <Box sx={{display: "flex", flexDirection: "column",  flexWrap: "nowrap", gap: "1rem", maxWidth: "80vw", justifySelf: "center"}}>
+            <Box sx={{display: "flex", justifyContent: "center", marginTop: "1rem"}}>
+                <Box sx={{ display: "flex", gap: 2, alignItems: "center", flexWrap: "wrap" }}>
+                    <Select
+                        value={selectedYear}
+                        onChange={e => setSelectedYear(e.target.value)}
+                        sx={{ minWidth: 100 }}
+                        size="small"
+                    >
+                        {years}
+                    </Select>
+                    <ButtonGroup sx={{display: "flex", flexWrap: "wrap"}}>
+                        {months.map((month, idx) => (
+                            <Button
+                                key={month}
+                                variant={selectedMonth === idx ? "contained" : "outlined"}
+                                onClick={() => setSelectedMonth(idx)}
+                            >
+                                {month.slice(0,3)}
+                            </Button>
+                        ))}
+                    </ButtonGroup>
+                </Box>
             </Box>
-        </Box>
-        <Box sx={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
-            <Box>
-                        <Box sx={{justifyContent: "center"}}>
-                            <p>Total orders: {reportValues.numOrders}</p>
-                            <p>Total sales: ${reportValues.sumOrders}</p>
-                            <p>Average order value: ${reportValues.avgOrders} </p>
-                        </Box>
-                        <Box>
-                            <Card>
-                                <CardHeader title="Most Popular Items"/>
-                                <CardContent>
-                                    <Typography>Size: {reportValues.topSize?.name}</Typography>
-                                    <Typography>Sauce: {reportValues.topSauce?.name}</Typography>
-                                    <Typography>Cheese: {reportValues.topCheese?.name}</Typography>
-                                    <Box>
-                                        <Typography>Toppings:</Typography>
-                                        <ol>
-                                            {reportValues.topToppings?.map(topping => (
-                                                <li key={topping.id}>{topping.name}</li>
-                                            ))}
-                                        </ol>
-                                    </Box>
-                                </CardContent>
-                            </Card>
+            <Box sx={{display: "flex", flexDirection: "row", flexWrap: "noWrap", minWidth: "100%", justifyContent: "spaceEvenly"}}>
+                <Box sx={{display: "flex", flexDirection: "column", minWidth: "50%", alignItems: "center"}}>
+                    <Box sx={{justifyContent: "center"}}>
+                        <p>Total orders: {reportValues.numOrders}</p>
+                        <p>Total sales: ${reportValues.sumOrders}</p>
+                        <p>Average order value: ${reportValues.avgOrders} </p>
+                    </Box>
+                    <Box>
+                        <Card>
+                            <CardHeader title="Most Popular Items"/>
+                            <CardContent>
+                                <Typography>Size: {reportValues.topSize?.name}</Typography>
+                                <Typography>Sauce: {reportValues.topSauce?.name}</Typography>
+                                <Typography>Cheese: {reportValues.topCheese?.name}</Typography>
+                                <Box>
+                                    <Typography>Toppings:</Typography>
+                                    <ol>
+                                        {reportValues.topToppings?.map(topping => (
+                                            <li key={topping.id}>{topping.name}</li>
+                                        ))}
+                                    </ol>
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                </Box>
+                <Box sx={{display: "flex", minWidth: "50%", justifyContent: "center"}}>
+                    <Card sx={{minWidth: "100%"}}>
+                        <CardHeader title="Daily Breakdown"/>
+                        <CardContent>
+                            <Box sx={{position: "relative", overflow: "auto", maxHeight: "50vh"}}>
+                                <List>
+                                    {reportValues.dailyStats?.map(day => (
+                                        <ListItem key={day.date}>
+                                            <ListItemText>
+                                                {day.date}: Orders: {day.orders};  Sales: {Number(day.sales).toLocaleString('en-us', {style: "currency", currency: "USD"})}
+                                            </ListItemText>        
+                                        </ListItem>
+                                    ))}
+                                </List>
 
-                        </Box>
-            </Box>
-            <Box>
-                <Card>
-                    <CardHeader title="Daily Breakdown"/>
-                    <CardContent>
-                        <Box sx={{overflow: "auto"}}>
-                            <ul>
-                                {reportValues.dailyStats?.map(day => (
-                                    <li key={day.date}>{day.date} : Orders: {day.orders};  Sales: {Number(day.sales).toLocaleString('en-us', {style: "currency", currency: "USD"})}</li>
-                                ))}
-                            </ul>
-                        </Box>
-                    </CardContent>
-                </Card>
 
+
+                            </Box>
+                        </CardContent>
+                    </Card>
+
+                </Box>
             </Box>
         </Box>
         </>
